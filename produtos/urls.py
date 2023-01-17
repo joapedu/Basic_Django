6cb9e6c48c267122f773d355225f771b08b0237e
joapedu <joaoeduardobraga2@gmail.com>
2023-01-17 11:33:09 -0300

    update route

--- produtos/urls.py
+++ produtos/urls.py
@@ -3,6 +3,11 @@ from . import views
 
 urlpatterns = [
     path('ver/', views.ver_produto, name="ver_produto"),
-    path('inserir/', views.inserir_produto, name="inserir_produto")
+    #http:www.|||.com/produtos/ver/
+    path('inserir/', views.inserir_produto, name="inserir_produto"),
+    #http:www.|||.com/produtos/inserir/
+    path('ola/', views.ola_produto, name="ola_produto")
+    #http:www.|||.com/produtos/ola/
+
     #endereço, linkView.Função, atribuir nome
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def ver_produto(request):
-    return HttpResponse('Estou no ver')
+    return render(request, 'ver_produto.html')
+    #http:www.|||.com/produtos/ver/
 
 def inserir_produto(request):
-    return HttpResponse('Estou no inserir')
+    return render(request, 'inserir_produto.html')
+    #http:www.|||.com/produtos/inserir/
+
+def ola_produto(request):
+    return render(request, 'ola_produto.html', {'nome': 'caio'})
+    #http:www.|||.com/produtos/ola/
