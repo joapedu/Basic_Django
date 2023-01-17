323290ade91cd9ea8766f88881b52b276b87b6ae
joapedu <joaoeduardobraga2@gmail.com>
2023-01-17 11:56:16 -0300

    update templates + add css

--- joao_django/settings.py
+++ joao_django/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os #concatenação de diretorios (staticfiles)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,7 +45,7 @@ ROOT_URLCONF = 'joao_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [os.path.join(BASE_DIR, 'templates/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
